f1a2f1bf8df4008c66fc1151c804f7a70c875522
Caihong <113153744+novak123456@users.noreply.github.com>
2022-10-10 18:17:28 -0900

    Add files via upload

---
+++ src/model/Books.java
@@ -0,0 +1,57 @@
+package model;
+
+
+public class Books {
+
+	private String bookName;
+	private int bookID;
+	private int isAvailable;    //if a book is available isAvailable == 1 , if not isAvailable == 0
+	private String lendTo; 
+	
+	//non-parameter constructor
+	public Books() {
+		super();
+	}
+	
+	public Books(String bookName,int bookID, int isAvailable,String lendTo) {
+		this.bookName = bookName;
+		this.bookID = bookID; 
+		this.isAvailable = isAvailable;
+		this.lendTo = lendTo;			
+	}
+	
+	
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public int getBookID() {
+		return bookID;
+	}
+
+	public void setBookID(int bookID) {
+		this.bookID = bookID;
+	}
+
+	public int isAvailable() {
+		return isAvailable;
+	}
+
+	public void setAvailable(int isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
+	public String getLendTo() {
+		return lendTo;
+	}
+
+	public void setLendTo(String lendTo) {
+		this.lendTo = lendTo;
+	}
+	
+	
+}//
