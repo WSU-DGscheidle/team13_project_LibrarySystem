1d753238ab1966ba781b86578a2de4565f7c22a5
Caihong <113153744+novak123456@users.noreply.github.com>
2022-10-10 19:49:41 -0900

    Add files via upload

---
+++ test/dao/BookDaoTest.java
@@ -0,0 +1,25 @@
+package dao;
+
+import java.awt.print.Book;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.Books;
+import model.User;
+import utility_public.DataBaseUtility;
+import org.junit.Test
+
+
+/**
+ * 
+ * @author 
+ *
+ */
+public class BookDaoTest {
+	@Test
+	public void test_returnBook() {
+		
+	
+	}
+
+}//
