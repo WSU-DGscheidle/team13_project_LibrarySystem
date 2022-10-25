85d95efb0e83f970bf4db29c7d5628c6ac5af3a8
Caihong <113153744+novak123456@users.noreply.github.com>
2022-10-25 06:48:52 -0900

    Add files via upload

---
+++ test/dao/UserDaoTest.java
@@ -0,0 +1,24 @@
+package dao;
+
+import java.awt.print.Book;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.Books;
+import model.User;
+import utility_public.DataBaseUtility;
+import org.junit.Test
+
+
+/**
+ * 
+ * @author 
+ *
+ */
+public class UserDaoTest {
+	@Test
+	public void test_login(Connection con,User user) {
+		//More codes will be added
+	}
+
+}//
