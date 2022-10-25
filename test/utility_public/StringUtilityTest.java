0b3e9f6793e1c86fffc7f329480a38d42f56c4ca
Caihong <113153744+novak123456@users.noreply.github.com>
2022-10-25 06:49:35 -0900

    Add files via upload

---
+++ test/utility_public/StringUtilityTest.java
@@ -0,0 +1,30 @@
+package dao;
+
+import java.awt.print.Book;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.Books;
+import model.User;
+import utility_public.DataBaseUtility;
+import org.junit.Test
+import utility_public;
+
+
+/**
+ * 
+ * @author 
+ *
+ */
+public class StringUtilityTest {
+	@Test
+	public void test_isNull(String str ) {
+		//More codes will be added
+	}
+	
+	@Test
+	public void test_isEmpty(String str){
+		//More codes will be added
+	}
+
+}//
