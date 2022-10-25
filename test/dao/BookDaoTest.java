cf699c44c66d82898ec0e105ed7e648457b4a531
Caihong <113153744+novak123456@users.noreply.github.com>
2022-10-25 06:47:29 -0900

    Update BookDaoTest.java

--- test/dao/BookDaoTest.java
+++ test/dao/BookDaoTest.java
@@ -17,8 +17,32 @@ import org.junit.Test
  */
 public class BookDaoTest {
 	@Test
-	public void test_returnBook() {
-		
+	public void test_returnBook(Connection con,int id) {
+		//More codes will be added
+	
+	}
+	
+	@Test
+	public void test_getTotalNum(Connection con){
+		//More codes will be added
+	
+	}
+	
+	@Test
+	public void test_delete(Connection con,Books book){
+		//More codes will be added
+	
+	}
+	
+	@Test
+	public void test_add(Connection con,Books book){
+		//More codes will be added
+	
+	}
+	
+	@Test
+	public void test_isAvailable(Connection con,int availableStatus,String bookName){
+		//More codes will be added
 	
 	}
 
