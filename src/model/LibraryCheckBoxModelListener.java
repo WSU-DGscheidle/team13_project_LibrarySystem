e5d269e7d204eb70d5025b99aed8d7162a7d8cc2
SkylerBG <gentner.7@wright.edu>
2022-11-16 21:02:45 -0500

    Added the database operation on the checkbox listener to tell the database if the book was available or not

--- src/model/LibraryCheckBoxModelListener.java
+++ src/model/LibraryCheckBoxModelListener.java
@@ -1,8 +1,13 @@
 package model;
 
+import java.sql.Connection;
+
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
+
+import dao.BookDao;
+import utility_public.DataBaseUtility;
 
 public class LibraryCheckBoxModelListener implements TableModelListener {
 
@@ -12,12 +17,17 @@ public class LibraryCheckBoxModelListener implements TableModelListener {
         int column = e.getColumn();
         if (column == 3) {
         	TableModel model = (TableModel) e.getSource();
-            if((Boolean) model.getValueAt(row, column)) {
-            	System.out.println("true");
-            } 
-            else {
-            	System.out.println("false");
-            }
+        	DataBaseUtility dbUtil = new DataBaseUtility(); 
+        	Connection con;
+        	try {
+				con = dbUtil.getCon();
+				
+				//This function modifies the database which satisfies that part of the requirement
+            	BookDao.update_isAvailable(con, ((Boolean)model.getValueAt(row, column)) ? 1 : 0, (String)model.getValueAt(row, 1));
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
         }
 	}
 
