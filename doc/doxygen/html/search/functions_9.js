5d171d4a101d68142ae7a10896ca61bc5c598c76
SkylerBG <gentner.7@wright.edu>
2022-12-01 19:21:53 -0500

    Added doxygen comments for AddBookForm.java

--- doc/doxygen/html/search/functions_9.js
+++ doc/doxygen/html/search/functions_9.js
@@ -7,7 +7,7 @@ var searchData=
   ['setdueday_4',['setdueDay',['../classmodel_1_1_books.html#afa3e992a6dfb61c9a4b6743b162d464d',1,'model::Books']]],
   ['setlendto_5',['setLendTo',['../classmodel_1_1_books.html#a4e27d1ea85e42a321383276b65c68660',1,'model::Books']]],
   ['setpassword_6',['setPassword',['../classmodel_1_1_user.html#aea6892aee94929a724acea844061fa70',1,'model::User']]],
-  ['settable_7',['setTable',['../classviews_1_1_add_book_form.html#a3743ccaa947e25c92805c02fff366f79',1,'views::AddBookForm']]],
+  ['settablemodel_7',['setTableModel',['../classviews_1_1_add_book_form.html#ac73f5f8ef9fd28a39fb9f04ab26658a7',1,'views::AddBookForm']]],
   ['setusername_8',['setUserName',['../classmodel_1_1_user.html#a81bbb76db969be3ba966440a2ce22cc4',1,'model::User']]],
   ['showform_9',['showForm',['../classviews_1_1_add_book_form.html#aa359d7467b94b8e50c5eb654ad8ebb33',1,'views::AddBookForm']]],
   ['showtabledata_10',['showTableData',['../classviews_1_1_user_view.html#af79116c6eca12e794e52ae2dc48cee2e',1,'views::UserView']]]
--- src/views/AddBookForm.java
+++ src/views/AddBookForm.java
@@ -15,6 +15,15 @@ import dao.BookDao;
 import model.Books;
 import utility_public.DataBaseUtility;
 
+/**
+ * @class AddBookForm
+ * 
+ * This class is the AddBookForm View Interface. <br>
+ * This class contains a window that allows the user to add a book to the database <br>
+ * 
+ * @author Skyler Gentner
+ *
+ */
 public class AddBookForm extends JFrame {
 
 	//Default serial version ID since this class extends JFrame
@@ -25,11 +34,19 @@ public class AddBookForm extends JFrame {
 	private JTextField textBookName, textBookAuthor;
 	private JLabel label, label_1, label_2;
 	
+	/**
+	 * Constructor<br>
+	 * Takes the tableModel that AdminView has and stores it so that it can be modified by the user
+	 * @param tableModel
+	 */
 	public AddBookForm(DefaultTableModel tableModel)
 	{
 		this.tableModel = tableModel;
 	}
-
+	
+	/**
+	 * This functions shows the AddBookForm and contains the functionality for the add book button which adds that book to the database
+	 */
 	public void showForm() {
 		frame1 = new JFrame("Add Book");
 		frame1.getContentPane().setLayout(new GridLayout(3, 2, 0, 0));
@@ -94,7 +111,7 @@ public class AddBookForm extends JFrame {
 		return tableModel;
 	}
 
-	public void setTable(DefaultTableModel tableModel) {
+	public void setTableModel(DefaultTableModel tableModel) {
 		this.tableModel = tableModel;
 	}
 
